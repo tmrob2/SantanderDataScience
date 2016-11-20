cfc9c155dee13a2f92cdd0875b00433dcb777776
tmrob2 <tmrob2@gmail.com>
2016-11-20 08:44:01 +0000

    Final updates to cleaning the data

--- SDS/InputData.py
+++ SDS/InputData.py
@@ -5,9 +5,10 @@ from sklearn import preprocessing
 import networkx as nx 
 import matplotlib.pyplot as plt
 import itertools
+import math
 
 class ImportData:
-    def __init__(self, vs: bool = False, set_dummies: bool=False, all_rows: bool=True):
+    def __init__(self, vs: bool = False, set_dummies: bool=False, all_rows: bool=True, scaling: bool=False):
         """
         The initialisation step will input the data into memory (data is not that large easily fits
         into RAM)
@@ -26,7 +27,12 @@ class ImportData:
         self.X = self.dataset.iloc[:, 0:24]
 
         # There seems to be 0.6% of rows that have NaN and this is ubiquitous among the feature set
-        self.X = self.X[self.X['age']!='NaN']
+        self.X = self.X[(self.X['age']!='NaN') & (self.X['age']!= 'NA') & (self.X['age']!=' NA')]
+        
+        # Get rid of the customer code. We want to identify patterns in customer behaviour, I think that
+        # matching on customer code will introduce some bias. We can always revisit this later.  
+        self.X.drop('ncodpers', axis=1, inplace=True)
+       
         # =======================================================================================
         #                                        CLEANING 
         # =======================================================================================
@@ -35,8 +41,12 @@ class ImportData:
         # Convert the dates to days from the beginning of the year or from now, whichever
         # makes more sense.
         # =======================================================================================
+        # Earnings
+        self.X.renta = self.X.renta.apply(lambda x: -99 if math.isnan(x) else x)
+        self.earn_ind = np.where(self.X.renta==-99)
+
         # Convert string to continuous where necessary
-        self.X['age'].apply(lambda x: np.float_(x) if not ' NA' else 0)
+        self.X['age'].apply(lambda x: np.float_(x))
         # Convert the record date into days and set the start as the beginning of 2015
         self.X['days_recorded_from_ny'] = self.X.fecha_dato.apply(lambda x:
                                                          (datetime.datetime.strptime(x, "%Y-%m-%d") -
@@ -90,9 +100,12 @@ class ImportData:
         self.X.conyuemp.fillna(0)
 
         # The dummy variables for the indrel1_1mes and tiprel_1mes will not work correctly until the
-        # following have been applied
+        # following have been applied. This creates recognisable and standadised dummy variables
         self.X.indrel_1mes = self.X.indrel_1mes.apply(lambda x: self.run_f(str(x)[0]))
-        
+        self.X.tiprel_1mes = self.X.tiprel_1mes.apply(lambda x: self.run_f2(str(x)[0]))
+        self.X.ind_empleado = self.X.ind_empleado.apply(lambda x: 'not_emp' if x=='N' else x)
+        self.X.ind_empleado = self.X.ind_empleado.apply(lambda x: self.run_f3(x))
+        self.X.segmento = self.X.segmento.apply(lambda x: 'none_type_work' if x=='NaN' else x)
 
         # =======================================================================================
         #                              SETTING THE DUMMY VARIABLES
@@ -109,7 +122,7 @@ class ImportData:
             # Attach all of the dummy variables to the feature set and delete the columns that
             # they came from
             self.X = pd.concat([self.X, d_employee_ind, d_account_type, d_customer_relation,
-                       d_channel, d_province, d_country_res, d_class])
+                       d_channel, d_province, d_country_res, d_class], axis=1)
 
             self.X.drop('indrel_1mes', axis=1, inplace=True)
             self.X.drop('ind_empleado', axis=1, inplace=True)
@@ -122,9 +135,15 @@ class ImportData:
         # ======================================================================================
         #                            SCALING THE CONTINUOUS VARIABLES
         # ======================================================================================
+        if scaling:
+            self.X.age = preprocessing.scale(self.X.age)
+            self.X.days_recorded_from_ny = preprocessing.scale(self.X.days_recorded_from_ny)
+            self.X.tenure_days_from_now = preprocessing.scale(self.X.tenure_days_from_now)
+            self.X.antiguedad = preprocessing.scale(self.X.antiguedad)
+            self.X.renta = preprocessing.scale(self.X.renta)
+            self.X.renta[self.earn_ind[0]] = -2
 
-        #preprocessing.scale(self.X.age)
-
+        #Set the target set
         self.Y = self.dataset.iloc[:, 25:-1]
 
     def return_list_of_cat_cols(self):
@@ -154,6 +173,36 @@ class ImportData:
             x= 'none_type'
         return x
     
+    def run_f2(self,input):
+        x = ''
+        if input=='I':
+            x = 'inactive_rel'
+        elif input=='A':
+            x = 'active_rel'
+        elif input=='P':
+            x = 'former_rel'
+        elif input=='R':
+            x = 'potential_rel'
+        else:
+            x= 'none_rel'
+        return x
+
+    def run_f3(self, input):
+        x = ''
+        if input=='not_emp':
+            x = 'not_emp'
+        elif input=='B':
+            x='ex_emp'
+        elif input=='F':
+            x='filial_emp'
+        elif input=='A':
+            x='active_emp'
+        elif input=='P' or input=='S':
+            x='pass_emp'
+        else:
+            x='none_type_emp'
+        return x
+    
     def create_adjacency_of_products(self):
         l = self.Y.shape[1]
         A = np.repeat(0, l**2).reshape(l, l)
@@ -176,12 +225,3 @@ class ImportData:
         plt.xticks(np.arange(0.5, len(df.columns), 1), df.columns, rotation='vertical') 
         plt.title('Product relationships visualisation')
         fig1.savefig('prod_adj.png')
-
-import pandas as pd
-df = pd.DataFrame(A, index = cls_data.Y.columns.values, columns = cls_data.Y.columns.values)
-fig1 = plt.figure(figsize=(6,6))
-plt.pcolor(df,cmap=plt.cm.Reds)
-plt.yticks(np.arange(0.5, len(df.index), 1), df.index)
-plt.xticks(np.arange(0.5, len(df.columns), 1), df.columns, rotation='vertical') 
-plt.title('Product relationships visualisation')
-fig1.savefig('prod_adj.png')
